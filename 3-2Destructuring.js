2ea8d4b1e628cdd2d83d4bfaf6c3fa53b8c9aa4a
abdullahalmamun7776-cell <abdullahalmamun7776@gmail.com>
2026-08-27 09:28:57 +0600

    Add JavaScript files

---
+++ 3-2Destructuring.js
@@ -0,0 +1,51 @@
+
+
+const student = {
+    Name : "Abdullah",
+    age : 23,
+    address : "Dhaka,Bangladesh"
+}
+
+//  Old system
+
+// const OldName = student.Name
+
+// New System 
+// const {age , address, Name} = student
+// console.log(address);
+
+
+const student1 = {
+    Name : "Abdullah",
+    age : 23,
+    address : {
+        city : "Dhaka",
+        zipCode : 1212
+    }
+}
+
+
+const {address:{zipCode}} = student1
+
+console.log(zipCode)
+
+
+const arr =  ["red","Green","Black"];
+const [first,sec,thi] = arr
+console.log(sec)
+
+
+const student2 = {
+    Name2 : "mamun",
+    age2 : 23,
+    address2 : {
+        city2 : "Dhaka",
+        zipCode2 : 1212
+    },
+    hobbies : ["fishing","Gardening",'swimming', "climbing"],
+}
+const [first2,second,third,four]=hobbies;
+
+const{Name2 , address2:{city2}, hobbies:[]}=student2;
+
+console.log(second)
---
+++ literal.js
@@ -0,0 +1,23 @@
+// Math 
+
+
+let price = 500;
+let quantity = 5;
+
+// console.log(`Total Price : ${price*quantity}take pay the price to get it as your own`)
+
+// ?function Call
+
+function getDiscount (price){
+    return price*(20/100)
+}
+// console.log (`YOU Saved : ${getDiscount(price)} take Use 
+// this as an Investment`);
+// console.log (`YOU Saved : ${getDiscount(price)} take \n 
+// Your total bill is now${price*quantity -getDiscount(price)}Taka`);
+
+
+let stock = 5 ;
+
+console.log(`Status: ${stock >0 ?"In Stock"+ " " + stock: "Out of Stock"}`)
+// console.log(`Status: ${stock >0 ? "In Stock" : "Out of Stock"}`)
